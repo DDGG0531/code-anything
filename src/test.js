80948e44cd3365c54170e4d96e56691a94fa9501
JimDong <jimdong@damaiapp.com.tw>
2022-03-18 14:44:54 +0800

    change lint staged config

--- src/test.js
+++ src/test.js
@@ -1,3 +1,14 @@
-let foo = 'foo'
+let semi = 1 // semi 結尾分號
 
-console.log(foo)
+let singleQuote = 'single quote' // 單引號
+
+let trailingComma = {
+  some: 'value' // 逗點
+}
+
+let printWidth =
+  '012345678901234567890123456789012345678901234567890123456789xxx' // 一行最大寬度
+
+let arrowParens = x => {
+  return x
+} // 箭頭函數的括號，是不是要顯示
